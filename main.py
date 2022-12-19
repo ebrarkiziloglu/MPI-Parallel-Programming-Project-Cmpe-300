bc5a4e13fe389dee8db46aab07c49f09e45283bf
Ebrar <ebrark@Ebrars-MacBook-Pro.local>
2022-12-19 09:11:16 +0300

    Fix the number of lines issue

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ comm = MPI.COMM_WORLD
 rank = MPI.COMM_WORLD.Get_rank()
 num_ranks = MPI.COMM_WORLD.Get_size()
 
-# print(f"Number of processes is {num_ranks}")
 def merge_dict(main_dict, new_dict):
     for key in new_dict:
         if key in main_dict:
@@ -36,50 +35,45 @@ if rank == 0:
     with open(args.input_file, 'r') as file:
         lines = file.readlines()
     line_count = len(lines)
+    divison = line_count // (num_ranks - 1)
+    lines_per_worker = [divison for i in range(num_ranks - 1)]
+    remainder = line_count % (num_ranks - 1)
+    for i in range(remainder):
+        lines_per_worker[i] = lines_per_worker[i] + 1
+    print(f"number of the lines per worker is {lines_per_worker}")
 
-    print(f'Number of lines: {line_count}')
-    lines_per_worker = line_count // (num_ranks - 1)
-    if line_count % (num_ranks - 1):
-        lines_per_worker += 1
-    # print(f"number of the lines per worker is {lines_per_worker}")
-
+    index = 0
     for worker_rank in range(1, num_ranks):  # 1, 2, ..., num_rank-1
         list_of_lines = []
-        for j in range((worker_rank - 1) * lines_per_worker, worker_rank * lines_per_worker):
-            if j < line_count:
-                list_of_lines.append(lines[j])
+        for i in range(lines_per_worker[worker_rank - 1]):
+            list_of_lines.append(lines[index])
+            index += 1
         dict_of_worker_params[worker_rank] = list_of_lines
         comm.send(list_of_lines, dest=worker_rank)
 
     frequency = {}
-    total_frequency = 0
     if args.merge_method == "MASTER":
-        # Requirement 2
         for i in range(1, num_ranks):
             unigram = comm.recv(source=i, tag=1)
             frequency = merge_dict(frequency, unigram)
-        for value in frequency.values():
-            total_frequency += value
         #print(f"In the master, frequency is: \n\n{frequency}\n\n")
 
     elif args.merge_method == "WORKERS":
         frequency = comm.recv(source=num_ranks - 1)
         #print(f"In the worker, frequency is: \n\n{frequency}\n\n")
-        for value in frequency.values():
-            total_frequency += value
 
     with open(args.test_file, 'r') as test_file:
         test_lines = test_file.readlines()
         for bigram in test_lines:
             unigram = bigram.split()[0]
             bigram = unigram + " " + bigram.split()[1].split("\\")[0]
-            bigram_frequency = frequency[bigram] / total_frequency
-            unigram_frequency = frequency[unigram] / total_frequency
+            bigram_frequency = frequency[bigram]    # / total_frequency
+            unigram_frequency = frequency[unigram]  # / total_frequency
             print(f"Frequency of the bigram {bigram} is: {bigram_frequency / unigram_frequency}")
 
 else:
     if args.merge_method == "MASTER":
-        # Requirement 2 - Rukiye
+        # Requirement 2
         received_data = comm.recv(source=0)
         unigrams = {}
         bigrams = {}
@@ -107,7 +101,7 @@ else:
         comm.send(dict_of_master_params, dest=0, tag=1)
         pass
     elif args.merge_method == "WORKERS":
-        # Requirement 3 - Ebrar
+        # Requirement 3
         lines_to_parse = comm.recv(source=0)
         frequency = {}
         if rank > 1:
@@ -138,4 +132,3 @@ else:
             comm.send(frequency, dest=rank + 1)
         if rank == num_ranks - 1:
             comm.send(frequency, dest=0)
-        pass
