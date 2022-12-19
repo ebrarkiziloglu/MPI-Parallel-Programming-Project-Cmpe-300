98b75c9c1cc783feefee3b8221f465b522bc22bc
Ebrar <ebrark@Ebrars-MacBook-Pro.local>
2022-12-19 10:05:28 +0300

    Finalize the Code & add some comments.

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import argparse
 from mpi4py import MPI
 
-# mpiexec -n 7 --oversubscribe python3 main.py --input_file data/sample_text.txt --merge_method WORKERS --test_file data/test.txt
-# mpiexec -n 7 --oversubscribe python3 main.py --input_file data/sample_text.txt --merge_method MASTER --test_file data/test.txt
+# mpiexec -n 2 --oversubscribe python3 main.py --input_file data/sample_text.txt --merge_method WORKERS --test_file data/test.txt
+# mpiexec -n 2 --oversubscribe python3 main.py --input_file data/sample_text.txt --merge_method MASTER --test_file data/test.txt
 # mpiexec -n 4 python3 combined_project.py --input_file ./sample_text.txt --merge_method WORKERS --test_file ./test.txt
 
-# Add command line arguments:
+# Generate command line arguments:
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_file", type=str, help="The path of the input file")
 parser.add_argument("--merge_method", type=str, help="Type of the merging")
 parser.add_argument("--test_file", type=str, help="The path of the test file")
-# Parse the command line arguments
 args = parser.parse_args()
 
-# CHECK - Print the values of the command line arguments
-# print(f"Input file is {args.input_file}")
-# print(f"Merge method is {args.merge_method}")
-# print(f"Test file is {args.test_file}")
-
+# Create the MPI:
 comm = MPI.COMM_WORLD
 rank = MPI.COMM_WORLD.Get_rank()
 num_ranks = MPI.COMM_WORLD.Get_size()
@@ -32,7 +27,8 @@ def merge_dict(main_dict, new_dict):
 
 dict_of_worker_params = {}
 
-if rank == 0:
+if rank == 0:                # MASTER process
+    # Read the input file and divide the sentences into WORKERS and send the data:
     with open(args.input_file, 'r') as file:
         lines = file.readlines()
     line_count = len(lines)
@@ -51,15 +47,17 @@ if rank == 0:
         dict_of_worker_params[worker_rank] = list_of_lines
         comm.send(list_of_lines, dest=worker_rank)
 
+    # Receive the data according to the --merge_method argument:
     frequency = {}
     if args.merge_method == "MASTER":
         for i in range(1, num_ranks):
-            unigram = comm.recv(source=i, tag=1)
-            frequency = merge_dict(frequency, unigram)
+            worker_frequency = comm.recv(source=i)
+            frequency = merge_dict(frequency, worker_frequency)
 
     elif args.merge_method == "WORKERS":
         frequency = comm.recv(source=num_ranks - 1)
 
+    # Read the test file to calculate the frequencies:
     with open(args.test_file, 'r') as test_file:
         test_lines = test_file.readlines()
         for bigram in test_lines:
@@ -70,64 +68,38 @@ if rank == 0:
             print(f"Frequency of the bigram {bigram} is: {bigram_frequency / unigram_frequency}")
 
 else:
+    # Receive the data from the MASTER and calculate the frequencies:
     data = comm.recv(source=0)
     number_of_lines = len(data)
     print(f"The worker with rank {rank} received {number_of_lines} sentences.")
+    
+    worker_frequency = {}
+    for sentence in data:
+        words = sentence.split()
+        word_count = len(words)
+        for i in range(word_count - 1):
+            word = words[i]
+            if word in worker_frequency:
+                worker_frequency[word] = worker_frequency[word] + 1
+            else:
+                worker_frequency[word] = 1
+            combination = words[i] + " " + words[i + 1]  
+            if combination in worker_frequency:
+                worker_frequency[combination] = worker_frequency[combination] + 1
+            else:
+                worker_frequency[combination] = 1
+
+    # Send the data appropriately, according to the --merge_method argument:
     if args.merge_method == "MASTER":
         # Requirement 2
-        unigrams = {}
-        bigrams = {}
-        for sentence in data:
-            words = sentence.split()
-            for i in range(len(words)-1):
-                if words[i] in unigrams:
-                    unigrams[words[i]] = unigrams[words[i]] + 1
-                else:
-                    unigrams[words[i]] = 1
+        comm.send(worker_frequency, dest=0)
 
-                combination = words[i] + " " + words[i + 1]
-                if combination in bigrams:
-                    bigrams[combination] = bigrams[combination] + 1
-                else:
-                    bigrams[combination] = 1
-
-            unigram = words[len(words)-1]
-            if unigram in unigrams:
-                unigrams[unigram] = unigrams[unigram] + 1
-            else:
-                unigrams[unigram] = 1
-
-        dict_of_master_params = merge_dict(bigrams, unigrams)
-        comm.send(dict_of_master_params, dest=0, tag=1)
-        pass
     elif args.merge_method == "WORKERS":
         # Requirement 3
-        frequency = {}
         if rank > 1:
             prev_frequency = comm.recv(source=rank - 1)
-            frequency.update(prev_frequency)
-        for sentence in data:
-            my_words = sentence.split()
-            word_count = len(my_words)
-            for i in range(word_count - 1):
-                unigram = my_words[i]
-                if unigram in frequency:
-                    frequency[unigram] = frequency[unigram] + 1
-                else:
-                    frequency[unigram] = 1
-                bigram = unigram + " " + my_words[i + 1]
-                if bigram in frequency:
-                    frequency[bigram] = frequency[bigram] + 1
-                else:
-                    frequency[bigram] = 1
-
-            unigram = my_words[word_count - 1]
-            if unigram in frequency:
-                frequency[unigram] = frequency[unigram] + 1
-            else:
-                frequency[unigram] = 1
-        # print(f"For rank {rank}, frequency dictionary is\n\n{frequency}\n\n\n\n")
+            worker_frequency = merge_dict(worker_frequency, prev_frequency)
         if rank < num_ranks - 1:
-            comm.send(frequency, dest=rank + 1)
+            comm.send(worker_frequency, dest=rank + 1)
         if rank == num_ranks - 1:
-            comm.send(frequency, dest=0)
+            comm.send(worker_frequency, dest=0)
--- results.md
+++ results.md
@@ -1,4 +1,4 @@
-# 7 procecceros with WORKERS:
+#### 7 procecceros with WORKERS:
 ```sh
 The worker with rank 1 received 39406 sentences.
 The worker with rank 2 received 39406 sentences.
@@ -14,7 +14,7 @@ Frequency of the bigram boğaziçi üniversitesi is: 0.37272727272727274
 Frequency of the bigram bilkent üniversitesi is: 0.2222222222222222
 ```
 
-# 7 procecceros with MASTER:
+#### 7 procecceros with MASTER:
 ```sh
 The worker with rank 1 received 39406 sentences.
 The worker with rank 2 received 39406 sentences.
@@ -30,7 +30,35 @@ Frequency of the bigram boğaziçi üniversitesi is: 0.37272727272727274
 Frequency of the bigram bilkent üniversitesi is: 0.2222222222222222
 ```
 
-# 4 procecceros with WORKERS:
+#### 5 procecceros with WORKERS:
+```sh
+The worker with rank 1 received 59109 sentences.
+The worker with rank 2 received 59109 sentences.
+The worker with rank 3 received 59108 sentences.
+The worker with rank 4 received 59108 sentences.
+Frequency of the bigram pazar günü is: 0.4462962962962963
+Frequency of the bigram pazartesi günü is: 0.5966101694915255
+Frequency of the bigram karar verecek is: 0.010940919037199124
+Frequency of the bigram karar verdi is: 0.13216630196936544
+Frequency of the bigram boğaziçi üniversitesi is: 0.37272727272727274
+Frequency of the bigram bilkent üniversitesi is: 0.2222222222222222
+```
+
+#### 4 procecceros with MASTER:
+```sh
+The worker with rank 1 received 59109 sentences.
+The worker with rank 2 received 59109 sentences.
+The worker with rank 3 received 59108 sentences.
+The worker with rank 4 received 59108 sentences.
+Frequency of the bigram pazar günü is: 0.4462962962962963
+Frequency of the bigram pazartesi günü is: 0.5966101694915255
+Frequency of the bigram karar verecek is: 0.010940919037199124
+Frequency of the bigram karar verdi is: 0.13216630196936544
+Frequency of the bigram boğaziçi üniversitesi is: 0.37272727272727274
+Frequency of the bigram bilkent üniversitesi is: 0.2222222222222222
+```
+
+#### 4 procecceros with WORKERS:
 ```sh
 The worker with rank 1 received 78812 sentences.
 The worker with rank 2 received 78811 sentences.
@@ -43,11 +71,35 @@ Frequency of the bigram boğaziçi üniversitesi is: 0.37272727272727274
 Frequency of the bigram bilkent üniversitesi is: 0.2222222222222222
 ```
 
-# 4 procecceros with MASTER:
+#### 4 procecceros with MASTER:
 ```sh
 The worker with rank 1 received 78812 sentences.
 The worker with rank 2 received 78811 sentences.
 The worker with rank 3 received 78811 sentences.
+Frequency of the bigram pazar günü is: 0.4462962962962963
+Frequency of the bigram pazartesi günü is: 0.5966101694915255
+Frequency of the bigram karar verecek is: 0.010940919037199124
+Frequency of the bigram karar verdi is: 0.13216630196936544
+Frequency of the bigram boğaziçi üniversitesi is: 0.37272727272727274
+Frequency of the bigram bilkent üniversitesi is: 0.2222222222222222
+```
+
+#### 3 procecceros with WORKERS:
+```sh
+The worker with rank 1 received 118217 sentences.
+The worker with rank 2 received 118217 sentences.
+Frequency of the bigram pazar günü is: 0.4462962962962963
+Frequency of the bigram pazartesi günü is: 0.5966101694915255
+Frequency of the bigram karar verecek is: 0.010940919037199124
+Frequency of the bigram karar verdi is: 0.13216630196936544
+Frequency of the bigram boğaziçi üniversitesi is: 0.37272727272727274
+Frequency of the bigram bilkent üniversitesi is: 0.2222222222222222
+```
+
+#### 3 procecceros with MASTER:
+```sh
+The worker with rank 1 received 118217 sentences.
+The worker with rank 2 received 118217 sentences.
 Frequency of the bigram pazar günü is: 0.4462962962962963
 Frequency of the bigram pazartesi günü is: 0.5966101694915255
 Frequency of the bigram karar verecek is: 0.010940919037199124
